78ce83d262e59470e70aa3f1274ede213724db19
Younis ahmed <younisalx-swe@hotmail.com>
2023-10-29 06:07:59 -0700

    fixes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,6 +5,9 @@ Creates a Blueprint instance with `url_prefix` set to `/api/v1`.
 
 
 from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -14,4 +17,4 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
