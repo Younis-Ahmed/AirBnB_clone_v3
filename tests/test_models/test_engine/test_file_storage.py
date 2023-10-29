a81f8e5a30d926745b793e1ab9397f13a38d48d8
Younis ahmed <younisalx-swe@hotmail.com>
2023-10-29 04:53:57 -0700

    places_amentites

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -18,6 +18,7 @@ import json
 import os
 import pep8
 import unittest
+
 FileStorage = file_storage.FileStorage
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -25,6 +26,7 @@ classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
 
 class TestFileStorageDocs(unittest.TestCase):
     """Tests to check the documentation and style of FileStorage class"""
+
     @classmethod
     def setUpClass(cls):
         """Set up for the doc tests"""
@@ -120,7 +122,7 @@ class TestFileStorage(unittest.TestCase):
         storage = FileStorage()
         user = User(name="User1")
         user.save()
-        self.assertEqual(user, storage.get("User", user.id))
+        self.assertEqual(user, storage.get(User, user.id))
 
     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_count(self):
